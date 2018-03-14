fc2bbaf67a159d32e929a79fc031139dcf0d3ce5
nagamurali <prv.nagamuraligarisapati@gmail.com>
2018-03-14 18:07:33 +0530

    Merge origin/master
    Conflicts:
    	data/error_details.json

---
+++ application/views/administration/menu_form.php
@@ -0,0 +1,164 @@
+<!-- Page-Title -->
+<div class="wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="page-title-box">
+                    <div class="btn-group pull-right">
+                        <?php echo $this->breadcrumbs->show(); ?>
+                    </div>
+                    <h4 class="page-title"><?php echo $title; ?></h4>
+                </div>
+            </div>
+        </div>
+        <div class="card-box">
+            <div class="row">
+                <div class="col-md-12">
+                    <form id="MenuForm" method="post" action="" class="form-horizontal" novalidate="">
+                        <div class="box-body">
+                            <div class="col-md-5 col-sm-5 col-xs-12">
+                                <!--<input type="hidden" name="MenuId" value="">-->
+                                <div class="form-group">
+                                    <label for="name" class="col-sm-4 control-label lable-font">Menu Name<span class="text-danger">*</span></label>
+                                    <div class="col-sm-8">
+                                        <input type="text" name="name" id="name" value="" parsley-trigger="change" placeholder="Menu Name" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="displayName" class="col-sm-4 control-label lable-font">Display Name<span class="text-danger">*</span></label>
+                                    <div class="col-sm-8">
+                                        <input type="text" name="displayName" id="displayName" value="" parsley-trigger="change" placeholder="Display Name" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="description" class="col-sm-4 control-label lable-font">Menu Description<span class="text-danger">*</span></label>
+                                    <div class="col-sm-8">
+                                        <textarea name="description" id="description" parsley-trigger="change" class="form-control"></textarea>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="MenuIcon" class="col-sm-4 control-label lable-font">Menu Icon (Font Awesome Class Name)<span class="text-danger">*</span></label>
+                                    <div class="col-sm-8">
+                                        <input type="text" name="MenuIcon" id="MenuIcon" value=""  placeholder="Menu Icon" class="form-control">
+                                        <i class=""></i>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="menuColor" class="col-sm-4 control-label lable-font">Menu Color<span class="text-danger">*</span></label>
+                                    <div class="col-sm-8">
+                                        <input type="text" name="menuColor" id="menuColor" value="" parsley-trigger="change" placeholder="Menu Color" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="IsParentMenu" class="col-sm-4 control-label lable-font">Is Parent Menu ? <span class="text-danger">*</span></label> 
+                                    <div class="col-sm-8">
+                                        <input type="radio" name="IsParentMenu" id="IsParentMenu" value="1"  > &nbsp; YES &nbsp;&nbsp;
+                                        <input type="radio" name="IsParentMenu" id="IsChildMenu" value="0"  checked> &nbsp; NO
+                                    </div>
+                                </div>
+                                <div id="parent_form" style="display:none;">
+
+                                </div>
+                                <div id="child_form" style="display:none;">
+                                    <div class="col-md-3">
+                                        <div class="form-group row">
+                                            <label for="menu_id" class="col-md-4">Parent Menu List<span class="text-danger">*</span></label>
+                                            <div class="col-md-8">
+                                                <select id="menu_id" class="form-control input-sm" name="menu_id">
+                                                    <?php foreach ($parentmenu_list as $list) { ?>
+                                                        <option value="<?= $list['menu_id'] ?>"><?= $list['menu_name'] ?></option>
+                                                    <?php } ?>
+                                                </select>
+                                            </div>
+                                        </div>
+
+                                    </div>
+<!--                                    <div class="form-group" id="MenuURLBox" >
+                                        <label for="menuURL" class="col-sm-4 control-label lable-font">Menu URL (Ex: ControllerName/MethodName)<span class="text-danger">*</span></label>
+                                        <div class="col-sm-8">
+                                            <input type="text" name="menuURL" id="menuURL" value="" parsley-trigger="change" placeholder="Menu URL" class="form-control">
+                                        </div>
+                                    </div>-->
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="controllerid" class="col-sm-4 control-label lable-font">Roles<span class="text-danger">*</span></label>
+                                    <div class="col-sm-8">
+                                        <select id="controllerid" class="form-control input-sm" name="controllerid">
+                                            <?php foreach ($roles_list as $list) { ?>
+                                                <option value="<?= $list['roleid'] ?>"><?= $list['rolename'] ?></option>
+                                            <?php } ?>
+                                        </select>
+                                        <i class=""></i>
+                                    </div>
+                                </div>
+
+
+
+
+
+                                <div class="form-group">
+                                    <label for="menuOrder" class="col-sm-4 control-label lable-font">Menu Order (After)<span class="text-danger">*</span></label>
+                                    <div class="col-sm-8">
+                                        <select class="form-control" name="menuOrder" id="menuOrder" required>
+                                            <option value="">Select Menu Order</option>
+                                            <option value="0">First</option>
+                                            <?php foreach ($childtmenu_list as $list) { ?>
+                                                <option value="<?= $list['menu_id'] ?>"><?= $list['menu_name'] ?></option>
+                                            <?php } ?>
+
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group  m-b-0">
+                                    <label for="" class="col-sm-4 control-label lable-font">Status<span class="text-danger">*</span></label>
+                                    <div class="col-sm-8">
+                                        <label class="radio-inline radio-font">
+
+                                            <input type="radio" name="is_active"   value="Y" checked>Active
+                                        </label>
+                                        <label class="radio-inline radio-font">
+                                            <input type="radio" name="is_active"  value="N">InActive
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="form-group row ">
+                                    <div class=" col-md-4 col-md-offset-2">
+                                        <button type="button" class="btn btn-default waves-effect waves-light btn-sm" id="btnSubmit">
+                                            SUBMIT
+                                        </button>
+                                        <button type="reset" class="btn btn-inverse waves-effect m-l-5 btn-sm">
+                                            CANCEL
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- end page title end breadcrumb -->
+    </div> <!-- end container -->
+</div>
+<!-- end wrapper -->
+<script src="<?php echo base_url(); ?>assets/js/pages/administration/menu_js.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#child_form').show();
+        $("input[name='IsParentMenu']").click(function () {
+            var ParentMenuStatus = $("input[name='IsParentMenu']:checked").val();
+            if (ParentMenuStatus == 1) {
+                $('#parent_form').show();
+                $('#child_form').hide();
+            } else {
+                $('#parent_form').hide();
+                $('#child_form').show();
+            }
+        });
+        var _baseUrl = '<?php echo base_url(); ?>';
+        menujs.Load(_baseUrl);
+    })
+</script>
---
+++ assets/js/pages/administration/menu_grid.js
@@ -0,0 +1,65 @@
+var MenuGridJS = (function () {
+
+    var _url;
+    var _csrf_token_name;
+    var _csrf_hash;
+    var _tbldepartment;
+    var _Load = function (Url, csrf_token_name, csrf_hash) {
+        _url = Url;
+        _csrf_token_name = csrf_token_name;
+        _csrf_hash = csrf_hash;
+        _tbldepartment = $("#tbl_menus");
+        LoadMenus();
+    }
+    var LoadMenus = function () {
+        _tbldepartment.DataTable({
+            responsive: true,
+            "processing": true,
+            "serverSide": true,
+            "ajax": {
+                "url": _url + "Administration/getAllMenus",
+                "dataType": "json",
+                "type": "POST",
+                "data": {_csrf_token_name: _csrf_hash}
+            },
+            "order": [[1, 'asc']],
+            "columns": [
+                {mRender: function (data, type, row) {
+                        var link = "";
+
+                        link = '<input type="checkbox" name="menu_id[]" value="' + row.menu_id + '"/>';
+                        return link;
+                    }, orderable: false},
+                {"data": "menu_id", "orderable": true},
+                {"data": "menu_name", "orderable": true},
+                {"data": "display_name", "orderable": true},
+                {
+                    mRender: function (data, type, row) {
+                        var status = "";
+                        if (row.isactive == 'Y') {
+                            status = "<span class='text-success'>Active</span>";
+                        } else {
+                            status = "<span class='text-warning'>In-active</span>"
+                        }
+                        return status;
+                    }, orderable: false},
+//                {mRender: function (data, type, row) {
+//                        var result = "";
+//                        result += '<a href="' + _url + 'Department/addOrEdit/' + row.departmentid + '">Edit</a>';
+//                        result += '<a href="' + _url + 'Department/delete_department/' + row.departmentid + '">Delete</a>';
+//                        return result;
+//                    }, orderable: false},
+            ], "fnRowCallback": function (nRow, aData, iDisplayIndex) {
+//console.log("Hello");
+                var index = iDisplayIndex + 1;
+                $('td:eq(1)', nRow).html(index);
+                return nRow;
+            }
+
+
+
+        });
+    }
+
+    return {Load: _Load}
+})();
